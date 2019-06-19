e464ef18723b7100844583930ecec27195734f8d
Levid Rodriguez <levid.rodriguez@gmail.com>
2019-06-19 12:54:43 -0500

    Ok

--- sobelFilter.cu
+++ sobelFilter.cu
@@ -13,11 +13,12 @@ void sobelFilterCPU(cv::Mat srcImg, cv::Mat dstImg, const unsigned int width, co
 void sobelFilterOpenCV(cv::Mat srcImg, cv::Mat dstImg);
 
 __global__ void sobelFilterOpenCVGradXGPU(unsigned char* srcImg, unsigned char* dstImg, const unsigned int h, const unsigned int w){
-    cv::Mat TempMat = cv::Mat(h, w, CV_8UC1, srcImg);
-    cv::Mat TempMat2 = cv::Mat(h, w, CV_8UC1, dstImg);
-    cv::Sobel(TempMat, TempMat2, CV_16S, 1, 0, 3, 1, 0, cv::BORDER_DEFAULT);
-    cv::convertScaleAbs(TempMat2, TempMat2);
-    dstImg = TempMat2.data;
+    cv::Mat TempMat(h,w,CV_8UC1,srcImg,cv::Mat::AUTO_STEP);
+    cv::Mat TempMatX.data = srcImg; 
+    // cv::Mat TempMat2 = cv::Mat(h, w, CV_8UC1, dstImg);
+    // cv::Sobel(TempMat, TempMat2, CV_16S, 1, 0, 3, 1, 0, cv::BORDER_DEFAULT);
+    // cv::convertScaleAbs(TempMat2, TempMat2);
+    dstImg = TempMat.data;
 }
 
 // __global__ void sobelFilterOpenCVGradYGPU(unsigned char* srcImg, unsigned char* dstImg){
